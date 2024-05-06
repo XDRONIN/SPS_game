e3b3353a5429791138d82b072db4dcbdda351161
XDRONIN <akashprakashmattathil@gmail.com>
2024-05-06 12:51:05 +0530

    the game can be now played using buttons

--- index.js
+++ index.js
@@ -18,23 +18,33 @@ document.body.appendChild(div1);
 div1.appendChild(rbut);
 div1.appendChild(pbut);
 div1.appendChild(sbut);
-// function randomchoice() {
-//   let choices = ["rock", "paper", "scissors"];
-//   let choice = Math.floor(Math.random() * choices.length);
-//   return choices[choice];
-// }
-// function playgame() {
-//   let PWIN = 0;
-//   let CWIN = 0;
-//   let winner;
-//   for (let i = 0; i <= 4; i++) {
-//     winner = playround();
-//     if (winner == "player") {
-//       PWIN = PWIN + 1;
-//     } else if (winner == "computer") {
-//       CWIN = CWIN + 1;
-//     }
-//   }
+
+rbut.addEventListener("click", () => {
+  playgame("rock");
+});
+pbut.addEventListener("click", () => {
+  playgame("paper");
+});
+sbut.addEventListener("click", () => {
+  playgame("scissors");
+});
+function randomchoice() {
+  let choices = ["rock", "paper", "scissors"];
+  let choice = Math.floor(Math.random() * choices.length);
+  return choices[choice];
+}
+function playgame(playerChoice) {
+  let PWIN = 0;
+  let CWIN = 0;
+  let winner;
+
+  winner = playround(playerChoice);
+  if (winner == "player") {
+    PWIN = PWIN + 1;
+  } else if (winner == "computer") {
+    CWIN = CWIN + 1;
+  }
+}
 
 //   if (CWIN > PWIN) {
 //     console.log(`The Computer wins by ${CWIN} points`);
@@ -43,36 +53,33 @@ div1.appendChild(sbut);
 //   } else {
 //     console.log("TIE");
 //   }
-// }
 
-// function playround() {
-//   let playerChoice = prompt("ROCK , PAPER, SCISSORS");
-//   let compChoice = randomchoice();
-//   playerChoice = playerChoice.toLowerCase();
-//   if (playerChoice === "rock" && compChoice === "scissors") {
-//     console.log(`player:${playerChoice}`);
-//     console.log(`computer:${compChoice}`);
-//     console.log("player wins");
-//     return "player";
-//   } else if (playerChoice === "paper" && compChoice === "rock") {
-//     console.log(`player:${playerChoice}`);
-//     console.log(`computer:${compChoice}`);
-//     console.log("player wins");
-//     return "player";
-//   } else if (playerChoice === "scissors" && compChoice === "paper") {
-//     console.log(`player:${playerChoice}`);
-//     console.log(`computer:${compChoice}`);
-//     console.log("player wins");
-//     return "player";
-//   } else if (playerChoice === compChoice) {
-//     console.log(`player:${playerChoice}`);
-//     console.log(`computer:${compChoice}`);
-//     console.log("TIE");
-//   } else {
-//     console.log(`player:${playerChoice}`);
-//     console.log(`computer:${compChoice}`);
-//     console.log("Computer wins");
-//     return "computer";
-//   }
-// }
-// playgame();
+function playround(playerChoice) {
+  let compChoice = randomchoice();
+  playerChoice = playerChoice.toLowerCase();
+  if (playerChoice === "rock" && compChoice === "scissors") {
+    console.log(`player:${playerChoice}`);
+    console.log(`computer:${compChoice}`);
+    console.log("player wins");
+    return "player";
+  } else if (playerChoice === "paper" && compChoice === "rock") {
+    console.log(`player:${playerChoice}`);
+    console.log(`computer:${compChoice}`);
+    console.log("player wins");
+    return "player";
+  } else if (playerChoice === "scissors" && compChoice === "paper") {
+    console.log(`player:${playerChoice}`);
+    console.log(`computer:${compChoice}`);
+    console.log("player wins");
+    return "player";
+  } else if (playerChoice === compChoice) {
+    console.log(`player:${playerChoice}`);
+    console.log(`computer:${compChoice}`);
+    console.log("TIE");
+  } else {
+    console.log(`player:${playerChoice}`);
+    console.log(`computer:${compChoice}`);
+    console.log("Computer wins");
+    return "computer";
+  }
+}
